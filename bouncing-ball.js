b68763dd62b08a44f6ff90f3989b7741d13dec68
kellypolasek <fx2446@wayne.edu>
2016-03-03 16:41:14 -0500

    Completed-Bouncing-Ball-Daily
    Completed the specs for Bouncing Ball.

--- bouncing-ball.js
+++ bouncing-ball.js
@@ -15,24 +15,44 @@
 
 // the arguments to the constructor function should represent the BouncingBall's starting position
 var BouncingBall = function (startingX, startingY) {
-
+    this.radius = random (5,15);
+    this.position = new p5.Vector (startingX, startingY);
+    //this.speed = (x: 1, y: 1);
 };
 
 BouncingBall.prototype = {
 
   // initialize() is typically only called once, after the object is created.
   initialize: function () {
-
+    this.direction = new p5.Vector(random(-5,5),random(-5,5));
+    this.direction.setMag(2);
   },
 
   // update() should update the state of the object, moving it and updating any properties
   update: function () {
+  this.position.add(this.direction);
+  this.checkForBounce();
 
   },
 
   // display() should draw the object to the canvas, using p5 code
   display: function () {
-
+    this.radius = random (5,15);
+    fill(random(0,255),random(0,255),random(0,255), 75);
+    ellipse( this.position.x, this.position.y, this.radius * 2 , this.radius * 2);
   },
 
+  bounceX: function () {
+    this.direction.x = this.direction.x * -1;
+  },
+
+  bounceY: function () {
+    this.direction.y = this.direction.y * -1;
+  },
+  checkForBounce: function () {
+      if (this.position.x > width - this.radius / 2) this.bounceX();
+      if (this.position.x < this.radius / 2) this.bounceX();
+      if (this.position.y > height - this.radius / 2) this.bounceY();
+      if (this.position.y < this.radius / 2) this.bounceY();
+  }
 };
